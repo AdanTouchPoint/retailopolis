d5460e89d514295422823beb0e232ffa91d5a702
Adan Mijangos <adan.mijangos@touchpointmarketing.mx>
2026-03-31 13:24:51 -0600

    changed favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   title: "Retailopoly",
   description: "Juego de Retailopoly",
   icons: {
-    icon: "/img/logo.png",
+    icon: "/img/favicong.png",
   },
 };
 
@@ -31,10 +31,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <header className="absolute top-0 w-full flex justify-center py-6 sm:py-8 z-50 pointer-events-none">
-          <img 
-            src="/img/logo.png" 
-            alt="Getin Logo" 
-            className="h-10 sm:h-12 object-contain pointer-events-auto drop-shadow-sm" 
+          <img
+            src="/img/logo.png"
+            alt="Getin Logo"
+            className="h-10 sm:h-12 object-contain pointer-events-auto drop-shadow-sm"
           />
         </header>
         {children}
